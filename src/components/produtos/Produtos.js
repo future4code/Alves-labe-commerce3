8218baab89f0298acd60518c706e778402e4537e
Adrielli de Aguiar Gross <adrielli.gross@gmail.com>
2022-06-16 19:51:26 -0300

    Alterações em produtos

--- src/components/produtos/Produtos.js
+++ src/components/produtos/Produtos.js
@@ -1,14 +1,70 @@
+import UniversoProfundo from '../img/universo-profundo.png';
+import OdisseiaEspacial from '../img/os-registros-de-uma-notavel-odisseia-espacial.png';
+import AlgumLugar from '../img/em-algum-lugar-nas-estrelas.png';
+import EncontroRama from '../img/encontro-com-rama.png';
+import OFim from '../img/o-fim-de-todas-as-coisas.png';
+import Misterio from '../img/misterio-por-tras-das-nossas-origens.png';
+import GuiaDefinitivo from '../img/mochileiro-das-galaxias.png';
+import Atomo from '../img/atomo-ao-buraco.png';
+import HistoriaUniverso from '../img/historia-do-universo.png';
 
 
 
 const listaDeProdutos = [
 {
-    id: 1 ,
+    id: 1,
     nome: 'Universo Profundo' ,
     valor: 48.90,
-    img : '../img/universo-profundo.png',
-}
-
+    img : UniversoProfundo,
+},
+{
+    id: 2,
+    nome: 'Os Registros Estelares de uma Notável Odisseia Espacial',
+    valor: 50.47,
+    img: OdisseiaEspacial,
+},
+{
+    id: 3,
+    nome: 'Em Algum Lugar nas Estrelas',
+    valor: 48.80,
+    img: AlgumLugar,
+},
+{
+    id: 4,
+    nome: 'Encontro com Rama',
+    valor: 35.65,
+    img: EncontroRama,
+},
+{
+    id: 5,
+    nome: 'O Fim de Todas as Coisas',
+    valor: 69.36,
+    img: OFim,
+},
+{
+    id: 6,
+    nome: 'O Mistério por Trás das Nossas Origens',
+    valor: 49.12,
+    img: Misterio,
+},
+{
+    id: 7,
+    nome: 'O Guia Definitivo do Mochileiro das Galáxias',
+    valor: 59.30,
+    img: GuiaDefinitivo,
+},
+{
+    id: 8,
+    nome: 'Do Átomo ao Buraco Negro',
+    valor: 38.70,
+    img: Atomo,
+},
+{
+    id: 9,
+    nome: 'A História do Universo Para Quem Tem Pressa',
+    valor: 38.34,
+    img: HistoriaUniverso,
+},
 ]
 
 export default listaDeProdutos
