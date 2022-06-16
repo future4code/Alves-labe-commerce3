973d243db1153eac9bf01eb1e574db4babb84699
Arthur Grossi Fernandes <arthur17000@gmail.com>
2022-06-16 13:14:17 -0300

    Migrando produtos entre componentes

---
+++ src/components/Filtro.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class Filtro extends React.Component {
+    state = {
+        produtos: this.props.produtos
+
+    }
+    componentDidMount() {
+
+    }
+    render(){
+        return(<h1>oi</h1>)
+    }
+}
