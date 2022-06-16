512f623228e7719ac0d3e026d52f6c9ae2711907
Guilherme Tocci <gt-267@hotmail.com>
2022-06-16 20:26:30 -0300

    filtro

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import React from 'react';
-import { Component } from 'react';
-import styled from 'styled-components';
-import Carrinho from './components/Carrinho';
-import Filtro from './components/Filtro';
-import Home from './components/Home';
-import listaDeProdutos from './components/produtos/Produtos';
+import React from "react";
+import { Component } from "react";
+import styled from "styled-components";
+import Carrinho from "./components/Carrinho";
+import Filtro from "./components/Filtro";
+import Home from "./components/Home";
+import listaDeProdutos from "./components/produtos/Produtos";
 
 export default class App extends React.Component {
   state = {
-    produtos : listaDeProdutos
+    produtos: listaDeProdutos,
+  };
+
+  render() {
+    console.log(this.state.produtos);
+    return (
+      <div>
+        <Filtro produtos={this.state.produtos} />
+        <Home produtos={this.state.produtos} />
+        <Carrinho produtos={this.state.produtos} />
+      </div>
+    );
   }
-  
-  render(){
-    console.log(this.state.produtos)
-  return (
-
-    <div>
-     
-
-      <Filtro produtos={this.state.produtos}/> 
-      <Home produtos={this.state.produtos}/>
-      <Carrinho produtos={this.state.produtos}/>
-
-  
-    </div>
-  );
 }
-}
-
--- src/components/Filtro.js
+++ src/components/Filtro.js
@@ -1,14 +1,55 @@
+
 import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+const InputsContainer = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  gap: 10px;
+`;
 
 export default class Filtro extends React.Component {
-    state = {
-        produtos: this.props.produtos
+  state = {
+    produtos: this.props.produtos,
+    filtro: "",
+  };
+  onChangeInput = (event) => {
+    this.setState({ filtro: event.target.value });
+    console.log(this.state.filtro);
+  };
+componentDidUpdate(){
+    let listaCrescente = [...this.state.produtos];
+    listaCrescente = listaCrescente.sort(function (a, b) {
+      return a - b;
+    })
+    
+    let listaDecrescente = [...this.state.produtos];
+    listaDecrescente = listaDecrescente.sort(function (a, b) {
+        return b - a;
+    })
+    if(this.state.filtro === "crescente"){
+        this.setState({produtos: listaCrescente})
+    
+    }else if(this.state.filtro === "descrescente"){
+        this.setState({produtos: listaDecrescente})
+    }
+}
+  render() {
+   
+     
 
-    }
-    componentDidMount() {
-
-    }
-    render(){
-        return(<h1>oi</h1>)
-    }
+    return (
+        <>
+      <InputsContainer>
+        <label>Filtro</label>
+        <select value={this.state.filtro} onChange={this.onChangeInput}>
+          <option value="crescente">Crescente</option>
+          <option value="decrescente">Decrescente</option>
+        </select>
+      </InputsContainer>
+      
+     
+      </>
+    );
+  }
 }
