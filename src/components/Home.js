297fb69c4bfdda26059976802453508f33c85ae9
Arthur Grossi Fernandes <arthur17000@gmail.com>
2022-06-17 15:18:26 -0300

    Finalizações filtros e carrinhos

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,106 @@
 import React from "react";
-import Carrinho from './Carrinho';
+// import Carrinho from './Carrinho';
+import styled from "styled-components";
+
+const BoxCarrinho = styled.div`
+border : solid black;
+
+`
 
 
 export default class Home extends React.Component {
     state = {
-        produtos: this.props.produtos,
+        produtos: this.props.produtosHome,
         produtosCarrinho: [],
+        precofinal: 0,
     }
 
     componentDidUpdate() {
         console.log(this.state.produtosCarrinho)
+        this.atualizaHome()
 
     }
-
+    atualizaHome() {
+        if (this.state.produtos !== this.props.produtosHome) {
+            this.setState({ produtos: this.props.produtosHome })
+        }
+    }
     AdicionarCarrinho(id) {
-        const novoCarrinho = this.state.produtosCarrinho.concat(this.state.produtos.filter(
-            (objeto) => {
+        const novoCarrinho = this.state.produtosCarrinho
+            .concat(this.state.produtos.filter((objeto) => {
                 if (objeto.id === id) {
                     return true
                 } else { return false }
-            }
-        )
-        )
+            })
+                .map((objeto) => {
+                    return {
+                        ...objeto,
+                        id: Date.now()
+                    }
+                })
+            )
         this.setState({ produtosCarrinho: novoCarrinho })
     }
+    precoFinal() {
+        let soma = 0.0001
+        let preco = this.state.produtos.map((item) => { return item.valor })
+        for (let index = 0; index < preco.length; index++) {
+            soma += preco[index]
+
+        }
+
+        if (this.state.precofinal !== soma) {
+            this.setState({ precofinal: soma })
+        }
+    }
+    removeItem(id) {
+        const carrinhoRemovido = this.state.produtosCarrinho
+            .filter((objeto) => {
+                if (objeto.id !== id) {
+                    return true
+                } else { return false }
+            })
+        this.setState({ produtosCarrinho: carrinhoRemovido })
+    }
     render() {
-        const carrinhoNovo = this.state.produtosCarrinho
+        const carrinho = this.state.produtosCarrinho.map((item) => {
+            return (
+                <>
+                    <li>
+                        <h4>{item.nome}</h4>
+                        <p>{item.valor}</p>
+                    </li>
+                    <button onClick={() => this.removeItem(item.id)}>Remover</button>
+                </>
+            )
+        })
         return (
             <>
-            
-                <ul>
-                    {this.state.produtos.map((item) => {
-                        return (
-                            <div>
-                                <li>
-                                    <h4>{item.nome}</h4>
-                                    <img src={item.img} />
-                                    <p>R${item.valor}</p>
-                                </li>
-                                <button
-                                    onClick={() => this.AdicionarCarrinho(item.id)}
-                                >Adicionar ao Carrinho
-                                </button>
-                            </div>
-                        )
-                    })
+
+
+                {this.state.produtos.map((item) => {
+                    return (
+                        <div>
+                            <li>
+                                <h4>{item.nome}</h4>
+                                <img src={item.img} />
+                                <p>R${item.valor}</p>
+                            </li>
+                            <button
+                                onClick={() => this.AdicionarCarrinho(item.id)}
+                            >Adicionar ao Carrinho
+                            </button>
+                        </div>
+                    )
+                })
                 }
-                <Carrinho produtos={carrinhoNovo}/>
-                </ul>
+
+                <BoxCarrinho>
+                    <ul>
+                        {carrinho}
+                    </ul>
+                    <h5>Total: R${this.state.precofinal.toFixed(2)}</h5>
+                </BoxCarrinho>
             </>
         );
     }
