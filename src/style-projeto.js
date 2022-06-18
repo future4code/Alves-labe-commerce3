9723e901557d03ac71b3d9d82465ed7eb0ce00e4
Arthur Grossi Fernandes <arthur17000@gmail.com>
2022-06-18 20:25:11 -0300

    Edit Estilo

--- src/style-projeto.js
+++ src/style-projeto.js
@@ -6,10 +6,12 @@ import styled from "styled-components";
 // AQUI FICA O HEADER :D
 export const HeaderStyle = styled.div`
   display: flex;
-  align-items: start;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
+  padding-top:2vh;
   img {
-    width: 15vw
+    width: 12vw
   }
   text-transform: uppercase;
   font-weight: 700;
@@ -25,7 +27,7 @@ export const HeaderStyle = styled.div`
 
 // AQUI FICA O CARRINHO n.n
 export const BoxCarrinho = styled.div`
-  width: 30%;
+  width: 20%;
   border: solid 1px #B8CAD4;
   text-align: center;
   text-transform: uppercase;
@@ -50,20 +52,12 @@ export const DisplayHome = styled.div`
   gap : 2rem;
   align-items: center;
   justify-content: center;
-  width: 70%;
-  img {
-    width: 200px;
-    height: 420px;
-    margin: 0.5rem 0 3rem 0;
-    box-shadow:3px 3px 3px #1f1f1f;
-  }
-  h4{
-   line-break: loose;
-  }
+  max-width: 80%;
   text-align: center;
   text-transform: uppercase;
   color: #30317B;
   font-family: arial;
+  padding: 2%;
 `
 
 
@@ -74,7 +68,7 @@ export const InputsContainer = styled.div`
   flex-direction: column;
   width: 15%;
   gap: 10px;
-  border: solid 1px #B8CAD4;
+  border: solid 1px #ABABCB;
   select {
     border-top: none;
     border-left: none;
@@ -82,7 +76,7 @@ export const InputsContainer = styled.div`
     font-size: 110%;
     color: #30317B;
     font-weight: 700;
-    background-color: #ABABCB
+    background-color: #FFEBFA
   }
   label{
     height: 2rem;
@@ -97,7 +91,7 @@ export const InputsContainer = styled.div`
     font-size: 110%;
     color: #30317B;
     font-weight: 700;
-    background-color: #ABABCB
+    background-color: #FFEBFA
   }
 `
 
@@ -112,33 +106,54 @@ export const ProdutoStyle = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
- 
+  flex-direction: column; 
+  h4{ 
+    height: 50px;
+  }
+  button{ font-size:100%;
+     display: flex;
+     color: #30317B;
+     align-items: center;
+  }
 `
 
 // AQUI FICA O FOOTER :3
 export const FooterStyle = styled.footer`
   display: flex;
-  justify-content: right;
-  background-color: #ABABCB;
-  padding: 20px 20px
-  img {
-    width: 20px
-  }
+  justify-content: space-evenly;
+  background-color: #30317b;
+  padding: 30px;
+  color: white;
+  align-items: center;
+  ul{list-style: circle}
+  ol{list-style:none}
+  img{width: 40px;}
 `
 
 export const PaginaToda = styled.div `
   display: flex;
   flex-direction: row;
-  background-color:#ABABCB;
+  background-color: #FFEBFA;
 
 `
 export const BoxDescricao = styled.div `
  width: 40%;
 `
 export const CarrinhoIcone = styled.img `
-  width:10px;
+  width:20px;
 `
 export const ImagemProduto = styled.img `
-  width:15vw;
+ width: 15vw;
+ height: 45vh;
+ margin: 0.5rem 0 3rem 0;
+ box-shadow:3px 3px 3px #1f1f1f;
+`
+export const SeuCarrinhoStyle = styled.img `
+width: 2rem;
+
+`
+export const IconesRedes = styled.div`
+  display: flex;
+  gap: 10px;
+  
 `
