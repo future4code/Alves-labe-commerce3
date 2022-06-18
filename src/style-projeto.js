c74ad300f1de329f094736f8c8e39fd6d9b838b9
Arthur Grossi Fernandes <arthur17000@gmail.com>
2022-06-18 18:13:08 -0300

    Estilização filtros

--- src/style-projeto.js
+++ src/style-projeto.js
@@ -21,13 +21,7 @@ export const HeaderStyle = styled.div`
 `
 
 // AQUI FICA O FILTRO ^u^
-export const FiltroStyle = styled.div`
-  display: flex;
-  flex-direction: column;
-  background: #ABABCB;
-  height: 100%;
-  align-items: flex-start;
-`
+
 
 // AQUI FICA O CARRINHO n.n
 export const BoxCarrinho = styled.div`
@@ -43,20 +37,28 @@ export const CarrinhoStyle = styled.div`
   display: flex;
   width: 100%;
   align-self: end;
+  justify-items: center;
   justify-self: end;
 `
 
 // AQUI FICA O CORPO DOS PRODUTOS <3
 export const DisplayHome = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
   flex-wrap: wrap;
+  gap : 2rem;
   align-items: center;
   justify-content: center;
+  width: 70%;
   img {
     width: 200px;
     height: 420px;
-    margin: 3rem;
+    margin: 0.5rem 0 3rem 0;
     box-shadow:3px 3px 3px #1f1f1f;
+  }
+  h4{
+   line-break: loose;
   }
   text-align: center;
   text-transform: uppercase;
@@ -68,13 +70,41 @@ export const DisplayHome = styled.div`
 // AQUI FICA O BODY DO FILTRO :)
 export const InputsContainer = styled.div`
   display: flex;
-  flex-direction: end;
+  padding: 2rem 1rem;
+  flex-direction: column;
+  width: 15%;
   gap: 10px;
+  border: solid 1px #B8CAD4;
+  select {
+    border-top: none;
+    border-left: none;
+    height: 2rem;
+    font-size: 110%;
+    color: #30317B;
+    font-weight: 700;
+    background-color: #ABABCB
+  }
+  label{
+    height: 2rem;
+    font-size: 120%;
+    color: #30317B;
+    font-weight: 700;
+  }
+  input{
+    border-top: none;
+    border-left: none;
+    height: 2rem;
+    font-size: 110%;
+    color: #30317B;
+    font-weight: 700;
+    background-color: #ABABCB
+  }
 `
 
 export const Display = styled.div`
   display: flex;
-  flex-wrap: wrap;
+  width: 100%;
+ 
 `
 
 // AQUI FICA OS PRODUTOS *u*
@@ -83,9 +113,7 @@ export const ProdutoStyle = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  img { 
-    width:15vw
-  };
+ 
 `
 
 // AQUI FICA O FOOTER :3
@@ -99,4 +127,18 @@ export const FooterStyle = styled.footer`
   }
 `
 
+export const PaginaToda = styled.div `
+  display: flex;
+  flex-direction: row;
+  background-color:#ABABCB;
 
+`
+export const BoxDescricao = styled.div `
+ width: 40%;
+`
+export const CarrinhoIcone = styled.img `
+  width:10px;
+`
+export const ImagemProduto = styled.img `
+  width:15vw;
+`
